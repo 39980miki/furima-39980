551d588878d6c4c833619b3e6a50977fbdd8ac92
39980miki <9898miki.1998@gmail.com>
2023-11-06 17:36:19 +0900

    ログアウト機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :name, presence: true
+  validates :password, match(/[a-z\d]{6,}/i)
+
+  has_many :items
+  has_many :purchases
 end
