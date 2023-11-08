8ed2c4cc05f5456c8ee282d5f019bfc487514a0f
39980miki <9898miki.1998@gmail.com>
2023-11-08 15:12:59 +0900

    マイグレーションファイル訂正

--- db/migrate/20231102095121_create_addresses.rb
+++
@@ -1,8 +0,0 @@
-class CreateAddresses < ActiveRecord::Migration[7.0]
-  def change
-    create_table :addresses do |t|
-
-      t.timestamps
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_06_062540) do
-  create_table "addresses", charset: "utf8", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
+ActiveRecord::Schema[7.0].define(version: 2023_11_02_082054) do
   create_table "items", charset: "utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
