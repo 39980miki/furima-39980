dfd7eb9531bff704d3bbb26fc323238dff29352f
39980miki <9898miki.1998@gmail.com>
2023-11-09 16:11:45 +0900

    JavaScript の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,11 +11,10 @@ class Item < ApplicationRecord
   validates :image, :item_name, :description, :category_id, :charge_id, :prefecture_id, :etd_id, :price, presence: true
 
   #ジャンルの選択が「---」の時は保存できないための記述
-  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :charge_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :etd_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :category_id, :condition_id, :charge_id, :prefecture_id, :etd_id, numericality: { other_than: 1 , message: "can't be blank"}
+
+  #価格について
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
 
   belongs_to :user
   # has_one :purchase
