226c6f209042caaae6ba61cd918cc7a6f3306993
39980miki <9898miki.1998@gmail.com>
2023-11-20 16:42:34 +0900

    修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,7 +16,7 @@ set :rbenv_ruby, '3.2.0'
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
-                                  keys: ['~/.ssh/39980mik.pem'] 
+                                  keys: ['~/.ssh/39980miki.pem'] 
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
